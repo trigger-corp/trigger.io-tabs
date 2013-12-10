f17d28fb30c28eb8ee67c3f6ba9fc401a6802337
Connor Dunn <connor@trigger.io>
2013-12-10 17:29:35 +0000

    Update .gitignore and remove ignored files.

--- inspector/ios-inspector/ForgeModule/build/ForgeModule.build/Release-iphoneos/ForgeModule.build/Objects-normal/armv7s/tabs_modalWebViewController.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/james/Code/modules/tabs/inspector/ios-inspector/ForgeModule/tabs_modalWebViewController.m \
-  /Users/james/Code/modules/tabs/inspector/ios-inspector/ForgeModule/tabs_modalWebViewController.h
--- inspector/ios-inspector/ForgeModule/build/ForgeModule.build/Release-iphonesimulator/ForgeModule.build/Objects-normal/i386/tabs_API.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/james/Code/modules/tabs/inspector/ios-inspector/ForgeModule/tabs_API.m \
-  /Users/james/Code/modules/tabs/inspector/ios-inspector/ForgeModule/tabs_API.h \
-  /Users/james/Code/modules/tabs/inspector/ios-inspector/ForgeModule/tabs_modalWebViewController.h
