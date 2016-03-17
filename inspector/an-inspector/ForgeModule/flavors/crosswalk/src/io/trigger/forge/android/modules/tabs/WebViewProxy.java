109550c134118308175e7eee035e1c1af752b936
Antoine van Gelder <antoine@trigger.io>
2016-03-17 17:46:01 +0200

    Bump binaries

--- inspector/an-inspector/ForgeModule/flavors/crosswalk/src/io/trigger/forge/android/modules/tabs/WebViewProxy.java
+++ inspector/an-inspector/ForgeModule/flavors/crosswalk/src/io/trigger/forge/android/modules/tabs/WebViewProxy.java
@@ -16,6 +16,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
+import android.os.Build;
 import android.webkit.ValueCallback;
 
 import com.google.gson.JsonElement;
