trigger-corp/trigger.io-tabs

commit e2e23dfdcd930d9380177c140dd6a7aa37b7f785

:bug: Added handling for frame load interrupted error on WebView

inspector/ios-inspector/ForgeModule/tabs_modalWebViewController.m
@@ -493,6 +493,14 @@ static UIBarButtonItem *reload = nil;
                                               otherButtonTitles:nil];
         [alert show];
     }
+    if (error.code == 102) {
+        NSString *URLString = [error.userInfo objectForKey:@"NSErrorFailingURLStringKey"];
+        NSURL *failedRequestURL = [NSURL URLWithString:URLString];
+        // Determine if we want the system to handle it.
+        if ([[UIApplication sharedApplication]canOpenURL:failedRequestURL]) {
+            [[UIApplication sharedApplication]openURL:failedRequestURL];
+        }
+    }
     [ForgeLog w:[NSString stringWithFormat:@"Modal webview error: %@", error]];
     [[ForgeApp sharedApp] event:[NSString stringWithFormat:@"tabs.%@.loadError", task.callid] withParam:@{@"url": self.webView.request.URL.absoluteString, @"description": error.description}];
 }

inspector/ios-inspector/ForgeModule/tabs_ConnectionDelegate.h
@@ -1,5 +1,5 @@
 //
-//  tabs_LoginDialogDelegate.h
+//  tabs_ConnectionDelegate.h
 //  ForgeModule
 //
 //  Created by Antoine van Gelder on 2017/03/16.

inspector/ios-inspector/ForgeModule/tabs_ConnectionDelegate.m
@@ -1,5 +1,5 @@
 //
-//  tabs_LoginDialogDelegate.m
+//  tabs_ConnectionDelegate.m
 //  ForgeModule
 //
 //  Created by Antoine van Gelder on 2017/03/16.
@@ -212,6 +212,7 @@
     }
 
     NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse*)response;
+    
     NSString *status = [NSHTTPURLResponse localizedStringForStatusCode:httpResponse.statusCode];
     NSString *responseString = [NSString stringWithFormat:@"%@", httpResponse];
 
@@ -237,6 +238,7 @@
     
     // remember the current url until response body is loaded
     currentUrl = [NSURL URLWithString:responseURL];
+    
 }
 
 

commit 16ebfabda78ab8536ae634cbf918455f8bd13cb1

:bug: Fix auto-close handling via pattern
If a request fails with error.code == 102, the handling introduced in
e2e23df tries to open the url in an external app (in order to support
downloads and other interactions like adding calendar events).

This broke the auto close behaviour of the tabs-module, which uses the
same error.

So we exclude urls matching the pattern from the open-external-app
handling here.

inspector/ios-inspector/ForgeModule/tabs_modalWebViewController.h
@@ -67,6 +67,7 @@
 - (ForgeTask*)getTask;
 - (void)setReturnObj:(NSDictionary *)newReturnObj;
 - (void)setPattern:(NSString *)newPattern;
+- (BOOL)matchesPattern:(NSURL *)urlToCheck;
 - (void)cancel:(id)nothing;
 - (void)stringByEvaluatingJavaScriptFromString:(ForgeTask*)evalTask string:(NSString*)string;
 - (void)close;

inspector/ios-inspector/ForgeModule/tabs_modalWebViewController.m
@@ -326,6 +326,18 @@ static UIBarButtonItem *reload = nil;
     pattern = newPattern;
 }
 
+- (BOOL)matchesPattern:(NSURL *)urlToCheck {
+    if (pattern == nil) {
+        return NO;
+    }
+
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:pattern
+                                                                           options:NSRegularExpressionCaseInsensitive
+                                                                             error:nil];
+    NSString *urlStr = [urlToCheck absoluteString];
+
+    return [regex numberOfMatchesInString:urlStr options:0 range:NSMakeRange(0, [urlStr length])] > 0;
+}
 
 - (void)setTitleTintColor:(UIColor *)newTint {
     titleTint = newTint;
@@ -351,7 +363,6 @@ static UIBarButtonItem *reload = nil;
     // Return YES for supported orientations
     return YES;
 }
-
 
 - (BOOL)webView:(UIWebView *)myWebView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
     NSURL *thisurl = [request URL];
@@ -398,21 +409,22 @@ static UIBarButtonItem *reload = nil;
     }
 
     // check return pattern
-    if (pattern != nil) {
-        NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:pattern options:NSRegularExpressionCaseInsensitive error:nil];
-        if ([regex numberOfMatchesInString:[thisurl absoluteString] options:0 range:NSMakeRange(0, [[thisurl absoluteString] length])] > 0) {
-            returnObj = [NSDictionary dictionaryWithObjectsAndKeys:
-                         [thisurl absoluteString],
-                         @"url",
-                         [NSNumber numberWithBool:NO],
-                         @"userCancelled",
-                         nil];
-            
-            [[[ForgeApp sharedApp] viewController] dismissViewControllerAnimated:YES completion:nil];
-            [[[ForgeApp sharedApp] viewController] performSelector:@selector(dismissModalViewControllerAnimated:) withObject:[NSNumber numberWithBool:YES] afterDelay:0.5f];
+    if ([self matchesPattern:thisurl]) {
+        [ForgeLog w:[NSString stringWithFormat:@"Encountered url matching pattern, closing the tab now: %@", thisurl]];
+        returnObj = [NSDictionary dictionaryWithObjectsAndKeys:
+                [thisurl absoluteString],
+                @"url",
+                [NSNumber numberWithBool:NO],
+                @"userCancelled",
+                        nil];
 
-            return NO;
-        }
+        [[[ForgeApp sharedApp] viewController] dismissViewControllerAnimated:YES
+                                                                  completion:nil];
+        [[[ForgeApp sharedApp] viewController] performSelector:@selector(dismissModalViewControllerAnimated:)
+                                                    withObject:[NSNumber numberWithBool:YES]
+                                                    afterDelay:0.5f];
+
+        return NO;
     }
 
     // we're done if basic auth is not enabled
@@ -494,10 +506,12 @@ static UIBarButtonItem *reload = nil;
         [alert show];
     }
     if (error.code == 102) {
-        NSString *URLString = [error.userInfo objectForKey:@"NSErrorFailingURLStringKey"];
-        NSURL *failedRequestURL = [NSURL URLWithString:URLString];
+        NSString *urlStr = [error.userInfo objectForKey:@"NSErrorFailingURLStringKey"];
+        NSURL *failedRequestURL = [NSURL URLWithString:urlStr];
+
         // Determine if we want the system to handle it.
-        if ([[UIApplication sharedApplication]canOpenURL:failedRequestURL]) {
+        if (![self matchesPattern:failedRequestURL] && [[UIApplication sharedApplication]canOpenURL:failedRequestURL]) {
+            [ForgeLog w:[NSString stringWithFormat:@"Open url by external app: %@", urlStr]];
             [[UIApplication sharedApplication]openURL:failedRequestURL];
         }
     }